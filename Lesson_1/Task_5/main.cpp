d38ce8969d0c852644fcc2e93c185020b5cd540c
petrunI <igorpetruni65@gmail.com>
2021-10-21 20:16:28 +0300

    Fixed requirements mistake

--- Lesson_1/Task_5/main.cpp
+++ Lesson_1/Task_5/main.cpp
@@ -5,10 +5,12 @@ using namespace std;
 
 int main()
 {
-    double mass;
+    double mass, dist = 300;
     cout << "Enter mass of the ship: ";
     cin >> mass;
-    double fuel = round(mass / 3) - 2;
-    cout << fuel;
+    double fuel = (round(mass / 3) - 2) * dist;
+    if(fuel < 0){
+        cout << "Error: invalid input";
+    }else cout << "Required fuel: " << fuel;
     return 0;
 }
