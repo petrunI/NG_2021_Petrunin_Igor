127c752e492107d88fd34d180e7949caa93dffc0
petrunI <igorpetruni65@gmail.com>
2021-10-21 20:21:40 +0300

    Now it`s correct and done with switch

--- Lesson_1/Task_6/main.cpp
+++ Lesson_1/Task_6/main.cpp
@@ -5,15 +5,40 @@ using namespace std;
 
 int main()
 {
-    int m, ch = 0, n;
-    do{
-        cout << "Complete the mask: 255.255.255.";
-        cin >> m;
-        if(m >= 0 && m < 255) ch = 1;
-        else cout << "Invalid mask. Try again" << endl;
+    int mask, ip = 0;
+    cout << "Complete the mask: 255.255.255.";
+    cin >> mask;
+    switch(mask){
+        case 0:
+            ip = 256;
+            break;
+        case 128:
+             ip = 128;
+            break;
+        case 192:
+            ip = 64;
+            break;
+        case 224:
+            ip = 32;
+            break;
+        case 240:
+            ip = 16;
+            break;
+        case 248:
+            ip = 8;
+            break;
+        case 252:
+            ip = 4;
+            break;
+        case 254:
+            ip = 2;
+            break;
+        case 255:
+            ip = 1;
+            break;
+        default:
+            cout << "invalid input";
     }
-    while(ch != 1);
-    n = pow(2, 8) - 2 - m;
-    cout << "Number of available ip-addresses: " << n;
+    if(ip != 0) cout << "Number of available ip-addresses: " << ip;
     return 0;
 }
