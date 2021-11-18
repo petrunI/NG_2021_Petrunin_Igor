87765f7af8b52be911f9cf5a4709d69119e260af
petrunI <igorpetruni65@gmail.com>
2021-11-18 20:04:08 +0200

    fixed task 3.3

--- Lesson_3/Task_3/main.cpp
+++ Lesson_3/Task_3/main.cpp
@@ -9,19 +9,19 @@ int main()
 {
     srand(time(0));
 
-    int array1[4], array2[4],
+    int array1[4] = {0}, array2[4] = {0},
         num1 = 0, num2 = 0;
 
     for(int i = 0; i < 4; i++){
        array1[i] = rand() % 10;
+       cout << array1[i] << " ";
        array2[i] = rand() % 10;
+       cout << array2[i] << endl;
        num1 *= 10;
        num2 *= 10;
        num1 += array1[i];
        num2 += array2[i];
-       /*num1 += array1[i] * pow(10, i);
-       num2 += array2[i] * pow(10, i);
-       cout << array1[i] << " " << num1 << " " << array2[i] << " " << num2 << endl;*/
+
     }
     cout << "Got: " << num1 << " + " << num2 << endl
          << "Result: " << (num1 + num2);
